ec0e7d958e0116693a956a5b5643d79ee77985a5
Andrew Kraemer <98714291+andrewckraemer@users.noreply.github.com>
2022-02-19 23:06:19 -0600

    Update app.R
    updated code to add some more choices

--- app.R
+++ app.R
@@ -15,22 +15,26 @@ sallylisttoprint<-names(table(mander_non_na$species))
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Salamander Records Visualizer"),
+  headerPanel("Salamander Records Visualizer"),
+  h5("This app uses GBIF data (www.gbif.org; February 2022) to plot species distributions for each salamander species recorded in the United States"),
+  br(),
   
   # Sidebar for input data 
   sidebarLayout(
     sidebarPanel(
-      h4(selectInput('specieschoice', 'Species List',c(sallylisttoprint))),
+      h4(selectInput('specieschoice', 'Species List',c(sallylisttoprint),selected="Plethodon cinereus")),
       br(),
-      tags$h4("Most Recent Observation"),
-      #verbatimTextOutput("year",placeholder=F)
+      radioButtons("yearswitch","",c("All"="all","Since 1990"="modern","Since 2010"="recent")),
+      br(),
+      tags$h4("The most recent observation is:"),
       h4(textOutput("year"))
     ),
     # Show a map of the data
     mainPanel(
-    plotOutput("plot"),
+      plotOutput("plot"),
     )
-  )
+  ),
+  tags$a(href="https://github.com/andrewckraemer/Salamander-Records-Visualizer","Andrew Kraemer's R script for this widget")
 )
 
 
@@ -38,33 +42,40 @@ ui <- fluidPage(
 server <- function(input, output,session) {
 
   # subset a single species
-  observe({specieschoice<-input$specieschoice
-  mander_data<-filter(mander_non_na, species == specieschoice)
-  mostrecent<-max(mander_data$year)
-  output$year<-renderText(mostrecent)
-  countymap <- counties(cb = TRUE,resolution='20m')
-  longlat<-data.frame(mander_data$decimalLongitude, mander_data$decimalLatitude)
-  colnames(longlat)<-c('long','lat')
+  observe({
+    specieschoice<-input$specieschoice
+    yearswitch<-switch(input$yearswitch,all=1500,modern=1990,recent=2010)
+    mander_yearsub<-subset(mander_non_na,mander_non_na$year>yearswitch)
+    mander_data<-filter(mander_yearsub, species == specieschoice)
+    mostrecent<-max(mander_data$year)
+    output$year<-renderText(mostrecent)
+    countymap <- counties(cb = TRUE,resolution='20m')
+    longlat<-data.frame(mander_data$decimalLongitude, mander_data$decimalLatitude)
+    colnames(longlat)<-c('long','lat')
   
-  latlong_sf <- longlat %>%
-    st_as_sf(coords = c("long", "lat"), crs = st_crs(countymap))
+    latlong_sf <- longlat %>%
+      st_as_sf(coords = c("long", "lat"), crs = st_crs(countymap))
   
   # count salamander records per county
-  data_sf_summary <- countymap %>% 
-    mutate(counts = lengths(st_intersects(., latlong_sf))) 
-  data_sf_summary$counts<-ifelse(data_sf_summary$counts>0,log(data_sf_summary$counts),data_sf_summary$counts) #log-transform counts
+    data_sf_summary <- countymap %>% 
+      mutate(counts = lengths(st_intersects(., latlong_sf))) 
+    data_sf_summary$counts<-ifelse(data_sf_summary$counts>0,log(data_sf_summary$counts),data_sf_summary$counts) #log-transform counts
   
   # plot
-  output$plot <- renderPlot({
-    p<- ggplot(data_sf_summary) +
-      geom_sf(aes(fill=counts),size=0.01) +
-      scale_fill_distiller("Relative Abundance", palette="Spectral",labels = c()) +
+    output$plot <- renderPlot({
+      p<- ggplot(data_sf_summary) +
+      geom_sf(aes(fill=counts),size=0.07) +
+      scale_fill_distiller("low - abundance - high", palette="Spectral",labels =c(),guide=guide_colourbar(title.position='top')) +
       ggtitle(specieschoice)+
-      coord_sf(xlim = c(-127, -68), ylim = c(20, 55)) +theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-                                                             panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position =c(0.15,0.2),plot.title = element_text(size = 20, face = "italic"))
-    p
-    #   })
-  })
+      coord_sf(xlim = c(-127, -68), ylim = c(22, 55)) +
+      #ylab('Latitude')+xlab('Longitude')+
+      theme(axis.text.y=element_blank(),axis.ticks=element_blank(),
+            axis.title.x=element_blank(),axis.text.x=element_blank(),
+            axis.title.y=element_blank(),panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+            panel.background = element_blank(), axis.line = element_blank(), 
+            legend.direction='horizontal',legend.position =c(0.25,0.2),plot.title = element_text(size = 20, face = "italic"))
+     p
+    })
   })
 }
 
