5f982258b995830b7648373b1ef96125b3daebc9
Andrew Kraemer <98714291+andrewckraemer@users.noreply.github.com>
2022-02-21 13:42:46 -0600

    Update app.R

--- app.R
+++ app.R
@@ -4,33 +4,44 @@ library(dplyr)
 library(tigris) 
 library(plotly) 
 library(geodist)
+library(cdlTools)
 
-# Data pre-processing
+## Data pre-processing
 mander_data_full<-read.csv('mander_data.csv')
 mander_named<-mander_data_full[!(mander_data_full $species==""), ] #clean data - only include records identified to species
 mander_non_na <- mander_named %>% filter_at(vars(decimalLatitude, decimalLongitude, year),all_vars(!is.na(.))) #clean data - only include records with lat, long, and year
 sallylisttoprint<-names(table(mander_non_na$species))
+statesf<-states(resolution='20m')
+countysf <- counties(cb = TRUE,resolution='20m')
 
-# User Interface
+# Find best county per state (all salamander records)
+topCOstate<-st_read("topcountyperstate.shp")
+
+
+
+## User Interface
 ui <- fluidPage(
   
   # Application title
   headerPanel("Salamander Records Visualizer"),
-  h5("This app uses GBIF data (www.gbif.org; February 2022) to plot species distributions for each salamander recorded in the United States"),
+  h5("This app uses Global Biodiversity Information Facility data (www.gbif.org) to plot distributions for each salamander species recorded in the United States. Data downloaded February 2022."),
   br(),
   
   # Sidebar for input data 
   sidebarLayout(
     sidebarPanel(
-      h4(selectInput('specieschoice', 'Species List',c(sallylisttoprint),selected="Plethodon cinereus")),
-      br(),
+      h4(selectInput('specieschoice', 'Choose your favorite salamander',c(sallylisttoprint),selected="Plethodon cinereus")),
       radioButtons("yearswitch","",c("All"="all","Since 1990"="modern","Since 2010"="recent")),
       br(),
-      tags$h5("The average number of observations each year is:"),
+      tags$h5("Average observations each year:"),
       h5(textOutput("avgyear")),
-      br(),
       tags$h5("The most recent observation is:"),
-      h5(textOutput("year"))
+      h5(textOutput("year")),
+      br(),
+      h4(selectInput('statechoice', 'If I visit this state, where will I see the most salamanders?',c(Choose='', state.name))),
+      h4(textOutput("bestcounty")),
+      actionButton("do", "Run")
+      
     ),
     # Show a map of the data
     mainPanel(
@@ -45,7 +56,7 @@ ui <- fluidPage(
 server <- function(input, output,session) {
 
   # subset a single species
-  observe({
+  observeEvent(input$do,{
     specieschoice<-input$specieschoice
     yearswitch<-switch(input$yearswitch,all=1500,modern=1990,recent=2010)
     mander_yearsub<-subset(mander_non_na,mander_non_na$year>yearswitch)
@@ -58,26 +69,35 @@ server <- function(input, output,session) {
     meanyear<-ifelse(meanyear.temp==Inf,"No records :(",meanyear.temp)
     output$avgyear<-renderText(meanyear)
     
-    countymap <- counties(cb = TRUE,resolution='20m')
+    # count salamander SPECIES CHOICE records per county
     longlat<-data.frame(mander_data$decimalLongitude, mander_data$decimalLatitude)
     colnames(longlat)<-c('long','lat')
-  
     latlong_sf <- longlat %>%
-      st_as_sf(coords = c("long", "lat"), crs = st_crs(countymap))
-  
-  # count salamander records per county
-    data_sf_summary <- countymap %>% 
-      mutate(counts = lengths(st_intersects(., latlong_sf))) 
+      st_as_sf(coords = c("long", "lat"), crs = st_crs(countysf))
+    data_sf_summary <- countysf %>% 
+      mutate(counts = lengths(st_intersects(countysf,latlong_sf))) 
     data_sf_summary$counts<-ifelse(data_sf_summary$counts>0,log(data_sf_summary$counts),data_sf_summary$counts) #log-transform counts
-  
+
+    #find county with most records from chosen state
+    stateboundary<-subset(statesf,statesf$NAME==input$statechoice)
+    countystatefp<-fips(input$statechoice,to='FIPS')
+    topcounty<-subset(topCOstate, topCOstate$STATEFP==sprintf("%02d", countystatefp))
+    topcountyname<-paste(topcounty$NAME,"County")
+    output$bestcounty<-renderText(topcountyname)
+
+    
   # plot
     output$plot <- renderPlot({
       p<- ggplot(data_sf_summary) +
       geom_sf(aes(fill=counts),size=0.07) +
+      geom_sf(data=stateboundary,size=0.3,color="darkblue",fill=NA)+
+      geom_sf(data=topCOstate,size=0.3,color="darkblue",fill=NA)+
+      geom_sf(data=topcounty,size=0.3,fill="darkblue")+
       scale_fill_distiller("absent - common", palette="Spectral",labels =c(),guide=guide_colourbar(title.position='top')) +
       ggtitle(specieschoice)+
       coord_sf(xlim = c(-127, -68), ylim = c(22, 55)) +
-      #ylab('Latitude')+xlab('Longitude')+
+      #geom_point(aes(x = bestLong, y = bestLat), size = 3, 
+      #             shape = 22, fill = "darkred")
       theme(axis.text.y=element_blank(),axis.ticks=element_blank(),
             axis.title.x=element_blank(),axis.text.x=element_blank(),
             axis.title.y=element_blank(),panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
